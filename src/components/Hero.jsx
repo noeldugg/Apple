78cd8b4b365ce13b4d9cbfea34fb685bf82da462
Noel Duggan <noelduggan21@gmail.com>
2024-07-21 11:30:38 +0000

    Uodated files

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+import { heroVideo,smallHeroVideo } from "../utils"
+
+const Hero = () => {
+    useGSAP(() => {
+        gsap.to('#hero', { opacity:1, delay: 1.5 })
+    }, [])
+  return (
+<section className="w-full nav-height bg-black relative">
+    <div className="h-5/6 w-full flex-center flex-col">
+    <p id="hero" className="hero-title">iPhone 15 Pro</p>
+    <div className="md:w-10/12 w-9-12">
+    <video>
+        <source src={videoSrc} />
+    </video>
+    </div>
+    </div>
+</section>
+  )
+}
+
+export default Hero
